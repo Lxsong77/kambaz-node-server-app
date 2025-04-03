1eb8ef4f0db41aeff242e729157ffc4b47e9474c
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-04-02 23:16:37 -0400

    update module

--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -1,6 +1,13 @@
 import Database from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
 
+export function updateModule(moduleId, moduleUpdates) {
+  const { modules } = Database;
+  const module = modules.find((module) => module._id === moduleId);
+  Object.assign(module, moduleUpdates);
+  return module;
+}
+
 export function createModule(module) {
     const newModule = { ...module, _id: uuidv4() };
     Database.modules = [...Database.modules, newModule];
@@ -17,10 +24,7 @@ export function deleteModule(moduleId) {
   Database.modules = modules.filter((module) => module._id !== moduleId);
  }
 
-export function updateModule(moduleId, moduleUpdates) {
-  const { modules } = Database;
-  const module = modules.find((module) => module._id === moduleId);
-  Object.assign(module, moduleUpdates);
-  return module;
-}
+
+
+
 
