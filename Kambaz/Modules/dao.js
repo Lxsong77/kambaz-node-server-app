605930038c495f8bb9211d05b336d05722a17805
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-04-02 23:32:25 -0400

    1

--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -1,11 +1,23 @@
 import Database from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
 
+// export function updateModule(moduleId, moduleUpdates) {
+//   const { modules } = Database;
+//   const module = modules.find((module) => module._id === moduleId);
+//   Object.assign(module, moduleUpdates);
+//   return module;
+// }
 export function updateModule(moduleId, moduleUpdates) {
   const { modules } = Database;
-  const module = modules.find((module) => module._id === moduleId);
-  Object.assign(module, moduleUpdates);
-  return module;
+  const moduleIndex = modules.findIndex((module) => module._id === moduleId);
+  if (moduleIndex === -1) {
+    throw new Error("Module not found");
+  }
+  if (!moduleUpdates || typeof moduleUpdates !== 'object') {
+    throw new Error("Invalid module updates");
+  }
+  modules[moduleIndex] = { ...modules[moduleIndex], ...moduleUpdates };
+  return modules[moduleIndex];
 }
 
 export function createModule(module) {
--- Kambaz/Modules/route.js
+++ Kambaz/Modules/route.js
@@ -7,11 +7,26 @@ export default function ModuleRoutes(app) {
       const status = await modulesDao.deleteModule(moduleId);
       res.send(status);
     });
+    // app.put("/api/modules/:moduleId", async (req, res) => {
+    //   const { moduleId } = req.params;
+    //   const moduleUpdates = req.body;
+    //   const status = await modulesDao.updateModule(moduleId, moduleUpdates);
+    //   res.send(status);
+    // });
     app.put("/api/modules/:moduleId", async (req, res) => {
-      const { moduleId } = req.params;
-      const moduleUpdates = req.body;
-      const status = await modulesDao.updateModule(moduleId, moduleUpdates);
-      res.send(status);
+      try {
+        const { moduleId } = req.params;
+        const moduleUpdates = req.body;
+        
+        if (!moduleUpdates) {
+          return res.status(400).send("Missing module updates");
+        }
+        
+        const updatedModule = await modulesDao.updateModule(moduleId, moduleUpdates);
+        res.json(updatedModule);
+      } catch (error) {
+        res.status(404).send(error.message);
+      }
     });
 
   
