b170a6d7d85824cb5cc35e1db161c9390cb54c10
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-04-12 21:09:38 -0400

    fix assignment bug

--- Kambaz/Assignment/route.js
+++ Kambaz/Assignment/route.js
@@ -1,37 +1,46 @@
-import db from "../Database/index.js";
+import * as assignmentsDao from "./dao.js";
 
 export default function AssignmentRoutes(app) {
-    app.put("/api/assignments/:aid", (req, res) => {
-        const {aid} = req.params;
-        const assignmentIndex = db.assignments.findIndex(
-            (a) => a._id === aid);
-        db.assignments[assignmentIndex] = {
-            ...db.assignments[assignmentIndex],
-            ...req.body
-        };
-        res.sendStatus(204);
+    app.put("/api/assignments/:assignmentId", async (req, res) => {
+        const { assignmentId } = req.params;
+        const assignmentUpdates = req.body;
+        const status = await assignmentsDao.updateAssignment(assignmentId, assignmentUpdates);
+        res.send(status);
     });
 
-    app.delete("/api/assignments/:aid", (req, res) => {
-        const {aid} = req.params;
-        db.assignments = db.assignments.filter((a) => a._id !== aid);
-        res.sendStatus(200);
+    app.delete("/api/assignments/:assignmentId", async (req, res) => {
+        try {
+            const { assignmentId } = req.params;
+            const status = await assignmentsDao.deleteAssignment(assignmentId);
+            res.send(status);
+        } catch (error) {
+            res.status(404).send(error.message);
+        }
     });
 
-    app.post("/api/courses/:cid/assignments", (req, res) => {
-        const {cid} = req.params;
-        const newAssignment = {
-            ...req.body,
-            course: cid,
-            _id: new Date().getTime().toString(),
-        };
-        db.assignments.push(newAssignment);
-        res.send(newAssignment);
+    app.post("/api/courses/:cid/assignments", async (req, res) => {
+        try {
+            const { cid } = req.params;
+            const newAssignment = {
+                ...req.body,
+                course: cid,
+            };
+
+            const createdAssignment = await assignmentsDao.createAssignment(newAssignment);
+            res.json(createdAssignment);
+        } catch (error) {
+            res.status(400).send(error.message);
+        }
     });
 
-    app.get("/api/courses/:cid/assignments", (req, res) => {
-        const {cid} = req.params;
-        const assignments = db.assignments.filter((c) => c.course === cid);
-        res.json(assignments);
+    app.get("/api/courses/:cid/assignments", async (req, res) => {
+        try {
+            const { cid } = req.params;
+            const assignments = await assignmentsDao.findAssignmentsForCourse(cid);
+            res.json(assignments);
+        } catch (error) {
+            res.status(404).send(error.message);
+        }
     });
+
 }
