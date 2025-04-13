97cde9764c915a6df29dd7712a4a7a929e05dcf7
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-04-13 11:34:56 -0400

    6.2.2 setup mongoose

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from 'cors';
@@ -8,6 +9,10 @@ import ModuleRoutes from './Kambaz/Modules/route.js';
 import AssignmentRoutes from './Kambaz/Assignment/route.js';
 import session from "express-session";
 import "dotenv/config";
+
+const CONNECTION_STRING = 
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(
