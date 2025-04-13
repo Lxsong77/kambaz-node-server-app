65e499daea5f31e7ba2cc6eeaf807f829caf0243
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-04-13 18:28:35 -0400

    6.2.6.6 Updating a Document in MongoDB with Mongoose

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,8 +1,4 @@
 import model from "./model.js";
-// import { v4 as uuidv4 } from "uuid";
-// let { users } = db;
-
-
 
 export const createUser = (user) => {} // implemented later
 export const findAllUsers = () => model.find();
@@ -11,7 +7,8 @@ export const findUserByUsername = (username) =>
   model.findOne({ username: username });
 export const findUserByCredentials = (username, password) => 
   model.findOne({ username, password });
-export const findUsersByRole = (role) => model.find({ role: role }); // or just model.find({ role })
+export const findUsersByRole = (role) => 
+  model.find({ role: role }); // or just model.find({ role })
 export const findUsersByPartialName = (partialName) => {
   const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
   return model.find({
@@ -19,5 +16,7 @@ export const findUsersByPartialName = (partialName) => {
   });
 };
 
-export const updateUser = (userId, user) =>  model.updateOne({ _id: userId }, { $set: user });
-export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+export const updateUser = (userId, user) =>  
+  model.updateOne({ _id: userId }, { $set: user });
+export const deleteUser = (userId) => 
+  model.deleteOne({ _id: userId });
