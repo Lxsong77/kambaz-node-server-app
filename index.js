b3f71a14a3218cf62d80404aa6e598d0746a82a4
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-04-14 23:08:01 -0400

    try to fix cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from 'cors';
 import UserRoutes from "./Kambaz/Users/routes.js";
+import EnrollmentsRoutes from "./Kambaz/Enrollments/router.js";
 import CourseRoutes from './Kambaz/Courses/routes.js';
 import ModuleRoutes from './Kambaz/Modules/route.js';
 import AssignmentRoutes from './Kambaz/Assignment/route.js';
@@ -10,17 +11,38 @@ import session from "express-session";
 import "dotenv/config";
 
 const app = express();
-app.use(
-  cors({
-    credentials: true,
-    origin: [
-      process.env.NETLIFY_URL, 
-      "http://localhost:5173",
-      "http://localhost:4000",
-      "https://kanbas-react-web-app-candice.netlify.app"
-    ]
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: [
+//       process.env.NETLIFY_URL, 
+//       "http://localhost:5173",
+//       "http://localhost:4000",
+//       "https://kanbas-react-web-app-candice.netlify.app"
+//     ]
+//   })
+// );
+const corsOptions =
+  process.env.NODE_ENV === "development"
+    ? {
+        origin: true,
+        credentials: true,
+      }
+    : {
+        origin: function (origin, callback) {
+          const allowedOrigins = [
+            process.env.NETLIFY_URL,
+          ];
+          if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+          } else {
+            callback(new Error("Not allowed by CORS"));
+          }
+        },
+        credentials: true,
+      };
+
+app.use(cors(corsOptions));
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
@@ -38,6 +60,7 @@ app.use(session(sessionOptions));
   
 app.use(express.json());
 UserRoutes(app);
+EnrollmentsRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);   
