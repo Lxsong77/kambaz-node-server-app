a2f3505008ff01323e34717459cbf5de74e13816
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-04-14 22:47:38 -0400

    try fix bug

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(
     origin: [
       process.env.NETLIFY_URL, 
       "http://localhost:5173",
+      "http://localhost:4000",
       "https://kanbas-react-web-app-candice.netlify.app"
     ]
   })
