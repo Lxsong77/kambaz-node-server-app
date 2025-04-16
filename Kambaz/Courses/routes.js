592e908802b92f853bb56a3df412c17505e8b85a
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-04-16 17:28:35 -0400

    6.4.1.2 Inserting Courses into a Database

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -2,6 +2,11 @@ import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
 
 export default function CourseRoutes(app) {
+  app.post("/api/courses", async (req, res) => {
+    const course = await dao.createCourse(req.body);
+    res.json(course);
+  });
+ 
   app.post("/api/courses/:courseId/modules", async (req, res) => {
     const { courseId } = req.params;
     const module = {
