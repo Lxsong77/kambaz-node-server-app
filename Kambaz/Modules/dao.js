e566c10d62d4295cd1ca8b147b995cc1a9e7bf7c
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-04-16 18:07:38 -0400

    module

--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -8,32 +8,40 @@ import { v4 as uuidv4 } from "uuid";
 //   return module;
 // }
 export function updateModule(moduleId, moduleUpdates) {
-  const { modules } = Database;
-  const moduleIndex = modules.findIndex((module) => module._id === moduleId);
-  if (moduleIndex === -1) {
-    throw new Error("Module not found");
-  }
-  if (!moduleUpdates || typeof moduleUpdates !== 'object') {
-    throw new Error("Invalid module updates");
-  }
-  modules[moduleIndex] = { ...modules[moduleIndex], ...moduleUpdates };
-  return modules[moduleIndex];
+  return model.updateOne({ _id: moduleId }, moduleUpdates);
+  // const { modules } = Database;
+  // const moduleIndex = modules.findIndex((module) => module._id === moduleId);
+  // if (moduleIndex === -1) {
+  //   throw new Error("Module not found");
+  // }
+  // if (!moduleUpdates || typeof moduleUpdates !== 'object') {
+  //   throw new Error("Invalid module updates");
+  // }
+  // modules[moduleIndex] = { ...modules[moduleIndex], ...moduleUpdates };
+  // return modules[moduleIndex];
 }
+
+// export function createModule(module) {
+//     const newModule = { ...module, _id: uuidv4() };
+//     Database.modules = [...Database.modules, newModule];
+//     return newModule;
+//   }
 
 export function createModule(module) {
     const newModule = { ...module, _id: uuidv4() };
-    Database.modules = [...Database.modules, newModule];
-    return newModule;
+    return model.create(newModule);
   }
   
 export function findModulesForCourse(courseId) {
-  const { modules } = Database;
-  return modules.filter((module) => module.course === courseId);
+  return model.find({ course: courseId });
+  // const { modules } = Database;
+  // return modules.filter((module) => module.course === courseId);
 }
 
 export function deleteModule(moduleId) {
-  const { modules } = Database;
-  Database.modules = modules.filter((module) => module._id !== moduleId);
+  return model.deleteOne({ _id: moduleId });
+  // const { modules } = Database;
+  // Database.modules = modules.filter((module) => module._id !== moduleId);
  }
 
 
--- Kambaz/Modules/route.js
+++ Kambaz/Modules/route.js
@@ -1,6 +1,7 @@
 import Database from "../Database/index.js";
 import * as modulesDao from "./dao.js";
 
+
 export default function ModuleRoutes(app) {
     app.delete("/api/modules/:moduleId", async (req, res) => {
       const { moduleId } = req.params;
@@ -14,19 +15,11 @@ export default function ModuleRoutes(app) {
     //   res.send(status);
     // });
     app.put("/api/modules/:moduleId", async (req, res) => {
-      try {
-        const { moduleId } = req.params;
-        const moduleUpdates = req.body;
-        
-        if (!moduleUpdates) {
-          return res.status(400).send("Missing module updates");
-        }
-        
-        const updatedModule = await modulesDao.updateModule(moduleId, moduleUpdates);
-        res.json(updatedModule);
-      } catch (error) {
-        res.status(404).send(error.message);
-      }
+      const { moduleId } = req.params;
+      const moduleUpdates = req.body;
+      const status = await modulesDao.updateModule(moduleId, moduleUpdates);
+      res.send(status);
+   
     });
 
   
