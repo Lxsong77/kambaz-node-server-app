6d6040d1b33e39b610672858e24b60dc1fecd9ab
Candice <155860581+Lxsong77@users.noreply.github.com>
2025-04-16 20:29:34 -0400

    assignment

--- Kambaz/Assignment/dao.js
+++ Kambaz/Assignment/dao.js
@@ -1,35 +1,21 @@
-import Database from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
+import model from "./model.js";
 
 export function findAssignmentsForCourse(courseId) {
-    const { assignments } = Database;
-    return assignments.filter((assignment) => assignment.course === courseId);
+    return model.find({ course: courseId });
 }
 
 export function updateAssignment(assignmentId, assignmentUpdates) {
-    const { assignments } = Database;
-    const assignment = assignments.find((assignment) => assignment._id === assignmentId);
-    if (!assignment) {
-        throw new Error(`Assignment with ID ${assignmentId} not found`);
-    }
-
-    if (!assignmentUpdates || typeof assignmentUpdates !== "object") {
-        throw new Error("Invalid assignment updates");
-    }
-
-    Object.assign(assignment, assignmentUpdates);
-    return assignment;
+    return model.updateOne({ _id: assignmentId }, assignmentUpdates );
 }
     
 
 export function deleteAssignment(assignmentId) {
-    const { assignments } = Database;
-    Database.assignments = assignments.filter((assignment) => assignment._id !== assignmentId);
+    return model.deleteOne({ _id: assignmentId });
 }
 
 export function createAssignment(assignment) {
     const newAssignment = { ...assignment, _id: uuidv4() };
-    Database.assignments = [...Database.assignments, newAssignment];
-    return newAssignment;
+    return model.create(newAssignment);
 }
 
